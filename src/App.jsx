589714fa8d30c133d516618574f91921f289ef01
ShreyAswal <aswalshrey@gmail.com>
2024-03-30 23:53:55 +0530

    API and fetch only relevant data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // import React from 'react'
+import { useState } from 'react'
 import './App.css'
+import useBookTitles from './Components/BookTitles'
 
 function App() {
+  const [input,setInput] = useState("")
+  const [pageNumber,setpageNumber] = useState(1);
+
+  useBookTitles(input,pageNumber)
+
+  function handleInput(e){
+    setInput(e.target.value)
+    setpageNumber(1)
+  }
+
   return (
     <div >
       <h1>Hello world</h1>
+      <input value={input} onChange={handleInput}></input>
+      {/* <useBookTitles/> */}
+      <h1>Loading...</h1>
     </div>
   )
 }
